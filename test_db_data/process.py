286fcd75549575042a70173887ea946f4974b585
Homyakin <tox.home@ya.ru>
2022-09-14 22:28:36 +0300

    [NF] add update entity in db

--- test_db_data/process.py
+++ test_db_data/process.py
@@ -3,30 +3,37 @@ from typing import List, Dict
 
 import psycopg2
 
+langs = {"ru": 1, "en": 2}
+
 conn = psycopg2.connect(dbname='seeker', user='dev',
                         password='dev', host='localhost')
 cursor = conn.cursor()
 
 
 def is_entity_present(table: str, data: Dict, pk_columns: List[str]) -> bool:
-    pk_values = {(pk, data[pk]) for pk in pk_columns}
-    pk_list = [f'{str(k)}={str(v)}' for k, v in pk_values]
-    pk_condition = ' and '.join(pk_list)
-    sql = f'SELECT * FROM {table} WHERE {pk_condition}'
-    cursor.execute(sql)
+    pk_template = ' and '.join([f'{column}=%s' for column in pk_columns])
+    fields = [str(data[column]) for column in pk_columns]
+    sql = f'SELECT * FROM {table} WHERE {pk_template}'
+    cursor.execute(sql, fields)
     return len(cursor.fetchall()) > 0
 
 
 def insert_entity(table: str, data: Dict, columns: List[str]):
     sql_columns = ', '.join(columns)
-    sql_values = ', '.join([str(data[column]) for column in columns])
-    sql = f'INSERT INTO {table} ({sql_columns}) VALUES ({sql_values})'
-    cursor.execute(sql)
+    template = ', '.join(['%s' for _ in range(len(columns))])
+    fields = [str(data[column]) for column in columns]
+    sql = f'INSERT INTO {table} ({sql_columns}) VALUES ({template})'
+    cursor.execute(sql, fields)
     conn.commit()
 
 
-def update_entity(table: str, data: Dict, columns: List[str], pk_columns: List[str]):
-    pass
+def update_entity(table: str, data: Dict, update_columns: List[str], pk_columns: List[str]):
+    pk_template = ' and '.join([f'{column}=%s' for column in pk_columns])
+    update_template = ', '.join([f'{column}=%s' for column in update_columns])
+    fields = [str(data[column]) for column in update_columns] + [str(data[column]) for column in pk_columns]
+    sql = f'UPDATE {table} SET {update_template} WHERE {pk_template}'
+    cursor.execute(sql, fields)
+    conn.commit()
 
 
 with open('test_db_data/data/events.json', encoding='utf-8') as json_file:
@@ -35,8 +42,18 @@ with open('test_db_data/data/events.json', encoding='utf-8') as json_file:
         print(element)
         if is_entity_present('event', element, ['id']):
             pass
+            # TODO для событий не надо
         else:
             insert_entity('event', element, ['id'])
+        locales = element['locale']
+        for locale in locales:
+            locale['event_id'] = element['id']
+            if 'lang' in locale:
+                locale['lang'] = langs[locale['lang']]
+            if is_entity_present('event_locale', locale, ['event_id', 'lang']):
+                update_entity('event_locale', locale, ['name'], ['event_id', 'lang'])
+            else:
+                insert_entity('event_locale', locale, ['event_id', 'lang', 'name'])
 
 cursor.close()
 conn.close()
