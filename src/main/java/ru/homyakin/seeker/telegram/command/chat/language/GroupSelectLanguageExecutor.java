dbad880b9b7eaaeef43223b9e08947603be7c253
Homyakin <tox.home@ya.ru>
2022-10-23 14:58:49 +0300

    [CF] remove admin check from group select language

--- src/main/java/ru/homyakin/seeker/telegram/command/chat/language/GroupSelectLanguageExecutor.java
+++ src/main/java/ru/homyakin/seeker/telegram/command/chat/language/GroupSelectLanguageExecutor.java
@@ -8,21 +8,17 @@ import ru.homyakin.seeker.locale.Language;
 import ru.homyakin.seeker.telegram.TelegramSender;
 import ru.homyakin.seeker.telegram.utils.Keyboards;
 import ru.homyakin.seeker.telegram.utils.TelegramMethods;
-import ru.homyakin.seeker.telegram.user.UserService;
 
 @Component
 public class GroupSelectLanguageExecutor extends CommandExecutor<GroupSelectLanguage> {
     private final ChatService chatService;
-    private final UserService userService;
     private final TelegramSender telegramSender;
 
     public GroupSelectLanguageExecutor(
         ChatService chatService,
-        UserService userService,
         TelegramSender telegramSender
     ) {
         this.chatService = chatService;
-        this.userService = userService;
         this.telegramSender = telegramSender;
     }
 
@@ -30,33 +26,15 @@ public class GroupSelectLanguageExecutor extends CommandExecutor<GroupSelectLang
     public void execute(GroupSelectLanguage command) {
         final var chat = chatService.getOrCreate(command.chatId());
         final var language = Language.getOrDefault(command.getLanguageId());
-        userService.isUserAdminInChat(command.chatId(), command.userId())
-            .peek(isAdmin -> {
-                    if (isAdmin) {
-                        final var updatedChat = chatService.changeLanguage(chat, language);
-                        telegramSender.send(
-                            TelegramMethods.createEditMessageText(
-                                command.chatId(),
-                                command.messageId(),
-                                Localization.get(updatedChat.language()).chooseLanguage(),
-                                Keyboards.languageKeyboard(updatedChat.language())
-                            )
-                        );
-                    } else {
-                        telegramSender.send(
-                            TelegramMethods.createAnswerCallbackQuery(
-                                command.callbackId(), Localization.get(chat.language()).onlyAdminAction()
-                            )
-                        );
-                    }
-                }
-            ).peekLeft(error ->
-                telegramSender.send(
-                    TelegramMethods.createAnswerCallbackQuery(
-                        command.callbackId(), Localization.get(chat.language()).internalError()
-                    )
-                )
-            );
+        final var updatedChat = chatService.changeLanguage(chat, language);
+        telegramSender.send(
+            TelegramMethods.createEditMessageText(
+                command.chatId(),
+                command.messageId(),
+                Localization.get(updatedChat.language()).chooseLanguage(),
+                Keyboards.languageKeyboard(updatedChat.language())
+            )
+        );
     }
 
 }
