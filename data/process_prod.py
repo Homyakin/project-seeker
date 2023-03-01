56a914bcd35aaaee684b01f961f7cf126a4afca7
Homyakin <tox.home@ya.ru>
2023-03-01 20:09:16 +0300

    [CF] add processing menu items to prod data

--- data/process_prod.py
+++ data/process_prod.py
@@ -1,9 +1,11 @@
 import threading
 
 import process_events
+import process_menu_items
 
 threads = [
-    threading.Thread(target=process_events.process_file, args=['project-seeker-prod-data'])
+    threading.Thread(target=process_events.process_file, args=['project-seeker-prod-data']),
+    threading.Thread(target=process_menu_items.process_file, args=['project-seeker-prod-data'])
 ]
 
 for i in threads:
