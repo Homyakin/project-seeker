228c371426a6a6ff2f61be2fb839fe022c4f328e
Homyakin <tox.home@ya.ru>
2023-03-11 14:57:28 +0300

    [CF] change telegram channel username

--- src/main/java/ru/homyakin/seeker/infrastructure/TextConstants.java
+++ src/main/java/ru/homyakin/seeker/infrastructure/TextConstants.java
@@ -15,6 +15,6 @@ public class TextConstants {
     public static final String DUEL_ACCEPT_ICON = ":dagger_knife:";
     public static final String DEFAULT_NAME = "Безымянный";
     public static final String SOURCE_LINK = "https://github.com/Homyakin/project-seeker";
-    public static final String TELEGRAM_CHANNEL_USERNAME = "@project_seeker_news";
+    public static final String TELEGRAM_CHANNEL_USERNAME = "@krezar_news";
     public static final String CALLBACK_DELIMITER = "~";
 }
