768034cf9b55977e2b4b32d3815404890a7b06fc
Homyakin <tox.home@ya.ru>
2024-12-30 13:26:08 +0400

    [CC] add badge for second season

--- src/main/java/ru/homyakin/seeker/game/personage/badge/BadgeView.java
+++ src/main/java/ru/homyakin/seeker/game/personage/badge/BadgeView.java
@@ -7,6 +7,7 @@ public enum BadgeView {
     STANDARD("standard", EmojiManager.getByAlias(":beginner:").orElseThrow().getEmoji()),
     FIRST_PERSONAGES("first-personages", EmojiManager.getByAlias(":reminder_ribbon:").orElseThrow().getEmoji()),
     FIRST_SEASON("first-season", EmojiManager.getByAlias(":otter:").orElseThrow().getEmoji()),
+    SECOND_SEASON("second-season", EmojiManager.getByAlias(":cat2:").orElseThrow().getEmoji()),
     ;
 
     private final String code;
