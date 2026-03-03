a4238a1f389556d8feb479a48620a08297be6bcc
Homyakin <tox.home@ya.ru>
2026-03-03 22:35:17 +0300

    [CC] new badge

--- src/main/java/ru/homyakin/seeker/game/badge/entity/BadgeView.java
+++ src/main/java/ru/homyakin/seeker/game/badge/entity/BadgeView.java
@@ -18,6 +18,7 @@ public enum BadgeView {
     FOURTH_SEASON("fourth-season", EmojiManager.getByAlias(":dog2:").orElseThrow().getFirst().getEmoji()),
     NEW_YEAR_2026("new-year-2026", EmojiManager.getByAlias(":sparkles:").orElseThrow().getFirst().getEmoji()),
     VALENTINE("valentine", EmojiManager.getByAlias(":two_hearts:").orElseThrow().getFirst().getEmoji()),
+    BUG("bug", EmojiManager.getByAlias(":beetle:").orElseThrow().getFirst().getEmoji()),
     ;
 
     private final String code;
