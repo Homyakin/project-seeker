4a5a6d6b3e2dc40a6f0a057ca9f1a19c7a7ce3e8
Homyakin <tox.home@ya.ru>
2026-04-10 22:00:19 +0300

    [CF] fix deploy

--- deploy-cloud.sh
+++ deploy-cloud.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 
-mvn package spring-boot:repackage && \
-docker build -t project-seeker . && \
-docker stop project-seeker 2>/dev/null; \
-docker rm project-seeker 2>/dev/null; \
+mvn package spring-boot:repackage
+docker build -t project-seeker .
+
+docker stop project-seeker 2>/dev/null || true
+docker rm project-seeker 2>/dev/null || true
+
 docker run -d \
   --name project-seeker \
   --network host \
