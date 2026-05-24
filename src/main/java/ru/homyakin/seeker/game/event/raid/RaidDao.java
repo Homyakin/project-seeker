b5de0452f2f894e39a9c09dc48de71fac2f8b205
Homyakin <tox.home@ya.ru>
2026-05-24 17:59:59 +0300

    [RF] remove legacy raids

--- src/main/java/ru/homyakin/seeker/game/event/raid/RaidDao.java
+++ src/main/java/ru/homyakin/seeker/game/event/raid/RaidDao.java
@@ -8,7 +8,6 @@ import javax.sql.DataSource;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Component;
 import ru.homyakin.seeker.game.event.raid.models.Raid;
-import ru.homyakin.seeker.game.event.raid.models.RaidTemplate;
 import ru.homyakin.seeker.infrastructure.init.saving_models.SavingRaid;
 import ru.homyakin.seeker.utils.JsonUtils;
 
@@ -26,10 +25,10 @@ public class RaidDao {
          WHERE event_id = :event_id
         """;
     private static final String SAVE = """
-        INSERT INTO raid (event_id, template_id, locale)
-        VALUES (:event_id, :template_id, :locale)
+        INSERT INTO raid (event_id, locale)
+        VALUES (:event_id, :locale)
         ON CONFLICT (event_id)
-        DO UPDATE SET template_id = :template_id, locale = :locale
+        DO UPDATE SET locale = :locale
         """;
     private final JdbcClient jdbcClient;
     private final JsonUtils jsonUtils;
@@ -55,7 +54,6 @@ public class RaidDao {
     public void save(int eventId, SavingRaid raid) {
         jdbcClient.sql(SAVE)
             .param("event_id", eventId)
-            .param("template_id", raid.template().id())
             .param("locale", jsonUtils.mapToPostgresJson(raid.locales()))
             .update();
     }
@@ -64,7 +62,6 @@ public class RaidDao {
         return new Raid(
             rs.getInt("event_id"),
             rs.getString("code"),
-            RaidTemplate.get(rs.getInt("template_id")),
             jsonUtils.fromString(rs.getString("locale"), JsonUtils.RAID_LOCALE)
         );
     }
